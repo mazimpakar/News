b417bf4ab3180b694db5648808f1cee141b976dd
mazimpakar <rosemazimpaka2@gmail.com>
2019-03-28 12:38:26 +0200

    first commit

---
+++ news/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import User,Article,tags
+# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
+admin.site.register(User)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(tags)
